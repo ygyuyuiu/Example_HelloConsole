a1c5921109774038ebb6dffc38fa6547ded61f9e
Egor Nagorny <e.nagorny97@bk.ru>
2023-02-25 14:18:41 +0300

    add Example 006 IfElse

---
+++ Example006_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello, bitch");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+     Console.WriteLine("fucking shit this is Маша");
+}
+else
+{
+     Console.Write("bitch, ");
+     Console.WriteLine(username);
+}
